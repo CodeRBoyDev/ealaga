e6fd4d0cad60b28431549f3cfd73ac5bc42964d0
Ricky Boy Donadillo <76178764+CodeRBoyDev@users.noreply.github.com>
2023-09-22 00:37:53 +0800

    Update axiosConfig.js

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -3,9 +3,10 @@ import axios from 'axios';
 // const API_URL = 'https://ealaga.vercel.app';
 // const API_URL = 'https://ealaga-5v97.onrender.com/';
 
-const API_URL = 'https://web-production-6235.up.railway.app/';
+//const API_URL = 'https://web-production-6235.up.railway.app/';
 // const API_URL = 'https://ealaga-server.onrender.com';
 // const API_URL = 'http://localhost:4000';
+const API_URL = 'http://192.168.100.17:4000'; 
 
 axios.defaults.baseURL = API_URL;
 
